f06bf012a0365f1808525af45509fbc8ac4aa054
HaeSeon <gildogi@naver.com>
2017-03-28 14:07:25 +0900

    Add files via upload

---
+++ src/src/headfirst/strategy/Duck.java
@@ -0,0 +1,30 @@
+package headfirst.strategy;
+
+public abstract class Duck {
+  private FlyBehavior fb;
+  private QuackBehavior qb;
+  
+  public void setFlyBehavior(FlyBehavior f){
+    this.fb=f;
+  }
+  
+  public void setQuackBehavior(QuackBehavior q){
+    this.qb=q;
+  }
+  
+  public void performFly(){
+    fb.fly();
+    System.out.println("All ducks fly");
+  }
+  
+  public void performQuack(){
+    qb.quack();
+    System.out.println("All ducks say quack");
+  }
+  
+  abstract void display();
+  
+  public void swim() {
+    System.out.println("All ducks swim");
+  }
+}
