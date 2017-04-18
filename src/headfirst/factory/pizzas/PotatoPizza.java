be039ec525eea3994a52d8f7d128c3e6c487bf94
HaeSeon <gildogi@naver.com>
2017-04-18 11:46:55 +0900

    Add files via upload

---
+++ src/headfirst/factory/pizzas/PotatoPizza.java
@@ -0,0 +1,15 @@
+package headfirst.factory.pizzas;
+
+
+public class PotatoPizza extends Pizza {
+ public PotatoPizza() {
+  name = "PotatoPizza Pizza";
+  dough = "Thin crust";
+  sauce = "Marinara Pizza Sauce";
+  toppings.add("Potato");
+  toppings.add("Parmesan");
+  toppings.add("Fresh Mozzarella");
+  toppings.add("Bacon");
+  toppings.add("mushroom");
+ }
+}
